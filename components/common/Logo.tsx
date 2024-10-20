1803855e29e58b4782271d8259525c535d7a333e
Nicolas Torres <nicotordev@gmail.com>
2024-10-20 15:04:31 -0300

    Add Logo component to display the site's logo

---
+++ components/common/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <div className="before:bg-pink-flamingo-600 before:w-5 before:h-5 before:rounded-full before:content-[''] before:block before:-z-10 relative flex items-center gap-2">
+      <h1>Nicotordev</h1>
+    </div>
+  );
+}
