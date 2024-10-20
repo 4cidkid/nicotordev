330c8862a738dffaddf57f9485ee17c74529ce04
nicotordev <nicotordev@gmail.com>
2024-10-20 16:09:44 -0300

    Refactor: Update Logo component to include a link to the home page

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Logo() {
   return (
-    <div className="before:bg-pink-flamingo-600 before:w-5 before:h-5 before:rounded-full before:content-[''] before:block before:-z-10 relative flex items-center gap-2">
-      <h1>Nicotordev</h1>
-    </div>
+    <Link href='/' className="before:bg-pink-flamingo-600 before:w-5 before:h-5 before:rounded-full before:content-[''] before:block before:z-10 relative flex items-center gap-2">
+      <h1 className="relative z-50">Nicotor<span className="text-pink-flamingo-600">dev</span></h1>
+    </Link>
   );
 }
