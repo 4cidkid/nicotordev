c6115c35380327f5ce94d2ca6245eb4e4ea01e92
Nicolas Torres <nicotordev@gmail.com>
2024-10-20 14:40:12 -0300

    Refactor: Add custom fonts and colors to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        title: "var(--font-montserrat)",
+        subtitle: "var(--font-raleway)",
+        body: "var(--font-lora)",
+        highlight: "var(--font-roboto)",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "pink-flamingo": {
+          "50": "#fef4ff",
+          "100": "#fde8ff",
+          "200": "#fad0fe",
+          "300": "#f9abfc",
+          "400": "#f667f9",
+          "500": "#ed45f0",
+          "600": "#d425d3",
+          "700": "#b01bac",
+          "800": "#90188c",
+          "900": "#751a70",
+          "950": "#4e044a",
+        },
       },
     },
   },
